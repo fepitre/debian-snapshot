a3307c3919739b4d6a0b5de9bf79eb2eb6f8d648
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2021-06-04 18:25:54 +0200

    cron: reduce window size

--- snapshot-mirror-cron.sh
+++ snapshot-mirror-cron.sh
@@ -6,7 +6,7 @@ SNAPSHOT_MOUNTPOINT=/snapshot
 
 # Adapt window of "1 day" with respect to cron
 NOW_TIMESTAMP="$(date -u +%Y%m%dT%H%M%SZ)"
-LOWER_TIMESTAMP="$(date -u +%Y%m%dT%H%M%SZ -d "1 day ago")"
+LOWER_TIMESTAMP="$(date -u +%Y%m%dT%H%M%SZ -d "12 hours ago")"
 
 exec {lock_fd}>/home/user/snapshot.lock || exit 1
 flock -n "$lock_fd" || { echo "ERROR: flock() failed." >&2; exit 1; }
