a3c246c6229d13c40e54467eaf164ce521d0c6e1
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2021-07-12 12:34:48 +0200

    list-timestamps: retry only for one archive
    (cherry picked from commit e319b59de936ecfc734fb0bbb857db714d1c9557)

--- scripts/list-timestamps.py
+++ scripts/list-timestamps.py
@@ -17,59 +17,64 @@ from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fi
     wait=wait_fixed(5),
     stop=stop_after_attempt(100),
 )
+def get_timestamps(archive, outdir):
+    months = []
+    with urllib.request.urlopen(
+            "http://snapshot.debian.org/archive/%s/" % archive
+    ) as f:
+        for line in f:
+            res = re.fullmatch(
+                r'<a href="\./\?year=(?P<year>\d+)&amp;month=(?P<month>\d+)">\d+</a>\n',
+                line.decode("utf-8"),
+            )
+            if res is None:
+                continue
+            months.append((int(res.group("year")), int(res.group("month"))))
+    assert len(months) > 0
+    timestamps = []
+    outfile = (outdir / archive).with_suffix(".txt")
+    lastmonth = None
+    if outfile.exists():
+        timestamps = outfile.read_text().splitlines()
+        lastmonth = (
+        int(timestamps[-1][:4]), int(timestamps[-1][4:6].lstrip("0")))
+
+    for year, month in months:
+        ts = []
+        # skip all months before the one of the last timestamp we have
+        if lastmonth is not None and (year, month) < lastmonth:
+            continue
+        with urllib.request.urlopen(
+                "http://snapshot.debian.org/archive/%s/?year=%d&month=%d"
+                % (archive, year, month)
+        ) as f:
+            for line in f:
+                res = re.fullmatch(
+                    r"<a href=\"(\d{8}T\d{6}Z)/\">\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d</a><br />\n",
+                    line.decode("utf-8"),
+                )
+                if res is None:
+                    continue
+                # if this month is the month with the last timestamp, skip
+                # those timestamps we already have
+                if (
+                        lastmonth is not None
+                        and (year, month) == lastmonth
+                        and res.group(1) in timestamps
+                ):
+                    continue
+                ts.append(res.group(1))
+        timestamps.extend(ts)
+    outfile.write_text("\n".join(timestamps) + "\n")
+
+
 def main():
     # FIXME: debian, debian-archive, debian-backports, debian-debug,
     #  debian-ports, debian-security, debian-volatile
     outdir = Path("/snapshot/by-timestamp")
     outdir.mkdir(exist_ok=True)
     for archive in ["debian", "debian-backports", "debian-ports", "debian-security"]:
-        months = []
-        with urllib.request.urlopen(
-            "http://snapshot.debian.org/archive/%s/" % archive
-        ) as f:
-            for line in f:
-                res = re.fullmatch(
-                    r'<a href="\./\?year=(?P<year>\d+)&amp;month=(?P<month>\d+)">\d+</a>\n',
-                    line.decode("utf-8"),
-                )
-                if res is None:
-                    continue
-                months.append((int(res.group("year")), int(res.group("month"))))
-        assert len(months) > 0
-        timestamps = []
-        outfile = (outdir / archive).with_suffix(".txt")
-        lastmonth = None
-        if outfile.exists():
-            timestamps = outfile.read_text().splitlines()
-            lastmonth = (int(timestamps[-1][:4]), int(timestamps[-1][4:6].lstrip("0")))
-
-        for year, month in months:
-            ts = []
-            # skip all months before the one of the last timestamp we have
-            if lastmonth is not None and (year, month) < lastmonth:
-                continue
-            with urllib.request.urlopen(
-                "http://snapshot.debian.org/archive/%s/?year=%d&month=%d"
-                % (archive, year, month)
-            ) as f:
-                for line in f:
-                    res = re.fullmatch(
-                        r"<a href=\"(\d{8}T\d{6}Z)/\">\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d</a><br />\n",
-                        line.decode("utf-8"),
-                    )
-                    if res is None:
-                        continue
-                    # if this month is the month with the last timestamp, skip
-                    # those timestamps we already have
-                    if (
-                        lastmonth is not None
-                        and (year, month) == lastmonth
-                        and res.group(1) in timestamps
-                    ):
-                        continue
-                    ts.append(res.group(1))
-            timestamps.extend(ts)
-        outfile.write_text("\n".join(timestamps)+"\n")
+        get_timestamps(archive, outdir)
 
 
 if __name__ == '__main__':
