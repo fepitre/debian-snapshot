9af4484db99dc74e774f0df595dfa63408247203
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2021-09-14 12:02:25 +0200

    backup: don't use sudo

--- scripts/snapshot-backup-db.sh
+++ scripts/snapshot-backup-db.sh
@@ -7,7 +7,7 @@ NOW_TIMESTAMP="$(date -u +%Y%m%dT%H%M%SZ)"
 exec {lock_fd}>/home/user/snapshot.lock || exit 1
 flock -n "$lock_fd" || { echo "ERROR: flock() failed." >&2; exit 1; }
 
-sudo -u postgres pg_dump snapshot
+pg_dump postgresql://snapshot:snapshot@127.0.0.1/snapshot \
     > "/usr/local/debian/snapshot-${NOW_TIMESTAMP}.psql"
 
 flock -u "$lock_fd"
